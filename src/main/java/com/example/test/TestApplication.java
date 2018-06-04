b24475130946a18b95db20a0bcac5a4350e38d75
BAOZUN\CSH10105 <1062281158@qq.com>
2018-06-04 17:30:01 +0800

    coco

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -9,8 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
 	public static void main(String[] args) {
-		//master
-		//dev
 		SpringApplication.run(TestApplication.class, args);
 	}
 }
